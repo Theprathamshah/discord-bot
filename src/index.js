fcb54ec8f1ca6bf570864a7919d70ec0215f2465
Pratham Shah <itspratham2911@gmail.com>
2024-12-02 13:58:47 +0530

    refactor: :recycle: removed unwanted changes

--- src/index.js
+++ src/index.js
@@ -10,27 +10,9 @@ connectDB()
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
-
-client.on('messageCreate', message => {
-    const lowerCaseMessage = message.content.toLowerCase();
-    if(lowerCaseMessage.includes('good night') && message.author.bot === false) {
-        message.react('🌜')
-        message.reply('Good Night borski!');
-    }
-})
-
 client.on('interactionCreate', async (interaction)=>{
     if(!interaction.isCommand()) return;
-    
-    if(interaction.commandName === 'ping') {
-        try {
-            console.log('Sending gifs!');
-            await interaction.reply('Pong you!!')
-            await interaction.channel.send('https://tenor.com/view/pong-gif-26462133')
-        } catch (error) {
-            console.error('Error sending GIF:', error)
-        }
-    }
+
     if (interaction.commandName === "verify") {
         await verifyUserProfile(interaction);
     }
